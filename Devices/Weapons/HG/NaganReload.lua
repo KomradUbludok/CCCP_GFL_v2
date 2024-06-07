e3e7c343f8554aa914b6cafc19cd23d5e5ad3331
Ubludok <KomradUbludok@gmail.com>
2024-06-07 19:12:53 +0300

    Changed folder name and paths in actors.ini

--- Devices/Weapons/HG/NaganReload.lua
+++ Devices/Weapons/HG/NaganReload.lua
@@ -32,31 +32,16 @@ function Update(self)
 		self.reloadCycle = false
 	end
 
-	if self.Magazine ~= nil then
-		self.ejectedShell = false
-	else
-		if self.ejectedShell == false then
-			self.ejectedShell = true
-			if self.HFlipped == false then
-				self.negativeNum = 1
-			else
-				self.negativeNum = -1
-			end
-		end
-	end
-
 	if self.reloadCycle == true and self.reloadTimer:IsPastSimMS(self.reloadDelay) and self:IsFull() == false then
 		local actor = MovableMan:GetMOFromID(self.RootID)
 		if MovableMan:IsActor(actor) then
 			ToActor(actor):GetController():SetState(Controller.WEAPON_RELOAD, true)
-			for i = 1, 1 do
-				local shell = CreateMOSParticle("Casing")
-				shell.Pos = self.Pos
-				shell.Vel = Vector(math.random() * -3 * self.negativeNum, 0)
-					:RadRotate(self.RotAngle)
-					:DegRotate((math.random() * 32) - 16)
-				MovableMan:AddParticle(shell)
-			end
+			local shell = CreateMOSParticle("Casing")
+			shell.Pos = self.Pos
+			shell.Vel = Vector(math.random() * -3 * self.negativeNum, 0)
+				:RadRotate(self.RotAngle)
+				:DegRotate((math.random() * 32) - 16)
+			MovableMan:AddParticle(shell)
 		end
 		self.reloadCycle = false
 	end
